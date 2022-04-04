264f50bf460d6bfc2878ee1f914019b5687ecf55
ToneAgency <brandon@tone.co.uk>
2022-04-04 17:07:02 +0100

    remove setup steps

--- pages/home.tsx
+++ pages/home.tsx
@@ -120,56 +120,6 @@ export default function Home({ }) {
         </CodeSnippet>
 
       </div>
-
-
-      <div className="steps_container mt-48">
-        {stepsData.map((item, i) => (
-          <StepItem item={item} key={i} />
-        ))}
-      </div>
     </Dashboard >
   );
 }
-
-const StepItem = ({ item: { title, status, link } }) => (
-  <Link href={link}>
-    <div className="rounded_shadow_box step_item">
-      <div>{title}</div>
-      <div style={{ color: colorStatus[status] || '#aaa' }}>{status}</div>
-    </div>
-  </Link>
-);
-
-const stepsData = [
-  {
-    title: 'Create and verify your Account',
-    status: 'done',
-    link: '/account/',
-  },
-  {
-    title: 'Create an API key',
-    status: 'done',
-    link: '/api-token/',
-  },
-  {
-    title: 'Make an API request',
-    status: 'waiting',
-    link: 'https://docs.speechmatics.com/en/cloud/howto/',
-  },
-  {
-    title: 'Set up payment',
-    status: 'waiting',
-    link: '/subscriptions/',
-  },
-  {
-    title: 'Review your usage',
-    status: '0.3h this month',
-    link: '/usage/',
-  },
-];
-
-const colorStatus = {
-  done: '#5BB4AE',
-  waiting: '#B49B5B',
-  'Need help?': '#5B8EB4',
-};
