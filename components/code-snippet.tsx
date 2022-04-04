ab81d10c97d2ed94459fa9c5931442e7a28f6c49
ToneAgency <brandon@tone.co.uk>
2022-04-04 17:13:37 +0100

    copy to clipboard with toast

--- components/code-snippet.tsx
+++ components/code-snippet.tsx
@@ -1,12 +1,29 @@
 import styles from '../styles/components/code-snippet.module.scss'
+import { useRef } from 'react'
+import { useToast } from '@chakra-ui/react'
 
 const CodeSnippet = ({ children }) => {
+    const codeRef = useRef(null);
+    const toast = useToast();
+
     return (
         <div className={styles.code_snippet}>
             <div className={styles.code_snippet__code}>
-                { children }
+                <span ref={codeRef}>{ children }</span>
             </div>
-            <button className='button button__grey button__small'>Copy</button>
+            <button className='button button__grey button__small' onClick={e => {
+                if( typeof window !== 'undefined' ){
+                    navigator.clipboard.writeText(codeRef.current.innerText);
+
+                    toast({
+                        title: 'Code Copied',
+                        description: 'This code has been copied to your clipboard.',
+                        status: 'success',
+                        duration: 6000,
+                        isClosable: true,
+                    })
+                }
+            }}>Copy</button>
         </div>
     )
 }
